5a07aa9759908377b6e12db3f560dfae11e79915
Rajan Kumar <raorajan9576@gmail.com>
2025-04-05 07:12:38 +0530

    database connected

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser"; 
+import connectDB from "./config/db.js";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("Server is Running! 🚀");
 });
