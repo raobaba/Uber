2cd14ffde95c998aa2d734518ad08a4ce0a3cf52
Rajan Kumar <raorajan9576@gmail.com>
2025-04-05 09:42:17 +0530

    swagger docs added

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser"; 
 import connectDB from "./config/db.js";
 import userRouter from "./routes/user.route.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+dotenv.config();
 
 const app = express();
+
+// Load Swagger YAML file
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+
 connectDB();
-app.use('/api/v1/user',userRouter)
+
+app.use('/api/v1/user', userRouter);
+
 app.get("/", (req, res) => {
   res.send("Server is Running! 🚀");
 });
